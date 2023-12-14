b3ccc22c08d23fe53c2dbf956891e8ec79d971fa
CHRISTO25 <christovarghese2725@gmail.com>
2023-12-14 15:08:54 +0530

    token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js';
 
 // @desc   Auth user/set token
 // route   POST/api/users/auth
@@ -9,11 +10,36 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 // @desc   Register a new user
-// route   POST/api/users/auth
+// route   POST/api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
+    const {name,email,password} = req.body
 
-    res.status(200).json({ message: 'Register User' });
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+     const user = await User.create({
+        name,
+        email,
+        password
+     });
+
+     if(user){
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+     }else{
+        res.status(400);
+        throw new Error('Invalid user data')
+     }
+
+    
 })
 
 // @desc   Logout User
