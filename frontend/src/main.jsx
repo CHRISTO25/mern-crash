81c6c16ff4c0a5fbc77767546ae63303115a8931
CHRISTO25 <christovarghese2725@gmail.com>
2024-01-06 15:13:46 +0530

    LoginSucess

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,14 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import store from './store.js'
+import { Provider } from 'react-redux'//bridge between react and redux
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 
 
@@ -17,11 +21,15 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
        <Route index={true} path='/' element={<HomeScreen/>}/>
+       <Route  path='/login' element={<LoginScreen/>}/>
+       <Route  path='/signup' element={<RegisterScreen/>}/>
     </Route>
   )
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
    <RouterProvider router={router}/>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>
 )
